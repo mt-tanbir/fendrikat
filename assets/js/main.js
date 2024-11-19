3a86df94e0fd917a682eb1afc0734bbb1c78b91a
mt-tanbir <97947584+mt-tanbir@users.noreply.github.com>
2024-11-19 17:50:49 +0600

    about me

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
         Meanmenu js / Mobile Menu Js
     =========================================================*/
 
-
   /* =========================================================
         Scroll To Top Start
     =========================================================*/
@@ -44,5 +43,46 @@ $(document).ready(function () {
     } else {
       $(".header").removeClass("sticky");
     }
+  });
+});
+// Slider
+$(document).ready(function () {
+  var $slider = $(".fe-slider-content");
+  var $progressBar = $(".fe-slide-progress");
+  var $progressBarLabel = $(".slider__label");
+
+  $slider.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+    var calc = (nextSlide / (slick.slideCount - 1)) * 100;
+
+    $progressBar
+      .css("background-size", calc + "% 100%")
+      .attr("aria-valuenow", calc);
+
+    $progressBarLabel.text(calc.toFixed(0) + "% completed");
+  });
+
+  $slider.slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 900,
+    arrows: false,
+    dots: false,
+    draggable: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 490,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
